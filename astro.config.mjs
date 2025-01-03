6a7c488165582cb78f681d5d16f32d885c8d568d
AldoRodriguez28 <arodriguez.ronquillo@hotmail.com>
2025-01-03 12:47:42 -0600

    refactorizando proyecto para crear build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
 
 export default defineConfig({
-    site: 'https://AldoRodriguez28.github.io',
-    base: 'agrupando-a-mexico',
-  })
+  site: 'https://AldoRodriguez28.github.io',
+  base: '/agrupando-a-mexico/',
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  vite: {
+    optimizeDeps: {
+      include: ['astro-icon'], // Precompila 'astro-icon'
+    },
+  },
+});
