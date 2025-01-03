dfc4b8d458875bf12de5f7d2cfa0ca2d1a1ebf62
AldoRodriguez28 <arodriguez.ronquillo@hotmail.com>
2025-01-02 21:56:58 -0600

    agregando configuracion para deploy con gihub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,6 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
-  output: 'static',
-  adapter: github({
-    repo: 'agrupando-a-mexico', // Reemplaza con el nombre de tu repositorio
-  }),
-  base: '/agrupando-a-mexico/', 
-});
+  site: 'https://AldoRodriguez28.github.io',
+  base: 'agrupando-a-mexico',
+})
