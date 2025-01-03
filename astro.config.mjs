1ce8f7d18f6e390f9740ecbe67e2382780c10446
AldoRodriguez28 <arodriguez.ronquillo@hotmail.com>
2025-01-02 22:20:30 -0600

    agrando iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['virtual:astro-icon'], // Marca el módulo como externo
+    },
+  },
   site: 'https://AldoRodriguez28.github.io',
   base: 'agrupando-a-mexico',
 })
